c3543c56a4dbee8d14d420746885f840f5fb376a
Robinson Lam <robinsonlam94@gmail.com>
2018-09-11 05:06:07 +1000

    remove .next files

--- .next/2.013daf71e1fc0fe75f1b.hot-update.js
+++
@@ -1,2 +0,0 @@
-webpackHotUpdate(2,[])
-//# sourceMappingURL=2.013daf71e1fc0fe75f1b.hot-update.js.map
--- .next/5.013daf71e1fc0fe75f1b.hot-update.js
+++
@@ -1,2 +0,0 @@
-webpackHotUpdate(5,[])
-//# sourceMappingURL=5.013daf71e1fc0fe75f1b.hot-update.js.map
